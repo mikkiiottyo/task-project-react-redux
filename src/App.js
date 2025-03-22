ecffd6a060200708ee891a1302b980363c3fbd79
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-21 23:28:37 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
           <TaskList/>
-        </p>
-        
       </header>
     </div>
   );

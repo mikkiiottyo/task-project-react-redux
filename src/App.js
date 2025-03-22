431b9021b079fb009562040d58cd79776bfa7eb6
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-22 00:25:17 -0800

    import addtask from addtask to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TaskList from './components/TaskList'; 
 import './App.css';
+import AddTask from './components/addTask'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <TaskList/>
+          <AddTask/>
       </header>
     </div>
   );
--- src/components/addTask.jsx
+++ src/components/addTask.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './addTask.css'
 
 const AddTask = () => {
     return (

f9c734208391f59311dbd3e9bbb18c40161db2fc
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-21 23:02:03 -0800

    Update TaskList.jsx
    exporting the default React component TaskList

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,2 +1,37 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+
+export default function TaskList() {
+    const tasks = useSelector((state) => state.tasks.tasks)
+    const loading = useSelector((state) => state.tasks.loading)
+    const error = useSelector((state) => state.tasks.error)
+
+    if(loading) {
+       return <p>Tasks Loading...</p>
+    }
+    if(error) {
+       return <p>There is an problem{error}</p>
+    }
+  return (
+    <div>
+      <div>
+        <h2>Tasks</h2>
+        <ul>
+            {tasks.map(task => (
+                <li key={task.id}>
+                    <div>
+                        <p>{task.title}</p>
+                        {task.description && <p>{task.description}</p>}
+                        <p>Status: {task.status}</p>
+                    </div>
+                    <div>
+                        <button>edit</button>
+                        <button>delete</button>
+                    </div>
+                </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  )
+}

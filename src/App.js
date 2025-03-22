c878de3dcc38ed609265e503b0022c74d41c3f72
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-21 23:05:47 -0800

    Update App.js
    defining App function

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <p>
+          <TaskList/>
+        </p>
+        
       </header>
     </div>
   );
 }
-
 export default App;

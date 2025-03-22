224a615778dd0ccab3b643d1d6e7b46f271160c8
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-22 14:30:48 -0800

    creating state values
    state values for title, description, status

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <TaskList/>
-          <AddTask/>
+        <AddTask/>
+        <TaskList/>
+          
       </header>
     </div>
   );
--- src/components/addTask.jsx
+++ src/components/addTask.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './addTask.css'
 
 const AddTask = () => {
+    const [title, setTitle] = useState ('')
+    const [description, setDescription] = useState('')
+    const [status, setStatus] = useState('')
     return (
         <form>
             <div>
                 <input
                 type="text"
                 placeholder="Task Name"
+                value={title}
+                onChange={ (e) => setTitle(e.target.value)}
                 className="form-container"
                 required
                 />
@@ -15,12 +20,17 @@ const AddTask = () => {
             <div>
                 <textarea
                 placeholder="Task Description"
+                value={description}
+                onChange={ (e) => setDescription(e.target.value)}
                 className="textarea"
                 rows="3"
                 ></textarea>
             </div>
             <div>
-                <select>
+                <select
+                value={status}
+                onChange={ (e) => setStatus(e.target.value)}
+                >
                     <option value="To Do">To Do</option> 
                     <option value="In Progress">In progress</option> 
                     <option value="Completed">Completed</option> 

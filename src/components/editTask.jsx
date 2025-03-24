5520aedfa8ba4441db94b7fa3e0b489d3ca1dd1e
mikkiiottyo <michaelaguirre15@gmail.com>
2025-03-24 14:23:00 -0800

    Update editTask.jsx

--- src/components/editTask.jsx
+++ src/components/editTask.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
 
 const EditTask = ({task,className}) => {
+const [isEdit, setIsEditing] = useState(false) 
+const [title, setTitle] = useState ('')
+const [description, setDescription] = useState('')
+const [status, setStatus] = useState('To Do')
+const dispatch = useDispatch()
   return (
     <div>
        <button className={className}>Edit</button>
